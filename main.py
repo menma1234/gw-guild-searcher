7b2f1e2e24edba65737362afba227dd55f303818
AC <acmenma@gmail.com>
2016-12-13 10:39:21 -0500

    git ignore

--- main.py
+++ main.py
@@ -79,4 +79,5 @@ def server_error(e):
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
 
-#app.run()
+if __name__ == '__main__':
+    app.run()
